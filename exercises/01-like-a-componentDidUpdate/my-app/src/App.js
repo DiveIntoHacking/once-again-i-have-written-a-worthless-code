76c9702420727d0f524392f33151ab4d569049fd
Atsushi Ishida <gipcompany@gmail.com>
2021-09-08 21:46:23 +0900

    implement sample code

--- exercises/01-like-a-componentDidUpdate/my-app/src/App.js
+++ exercises/01-like-a-componentDidUpdate/my-app/src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useRef } from 'react';
 
-function App() {
+const Area = ({ count, lastUpdatedAt, setCount }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>カウント {count} 回目</p>
+
+      <p>🎉最終カウントアップ日時🎉 {lastUpdatedAt || ''} </p>
+
+      <p>
+        <button onClick={() => setCount(count + 1)}>カウントアップ</button>
+      </p>
     </div>
   );
-}
+};
+
+const Qiita = () => {
+  const [count, setCount] = useState(0);
+  const [lastUpdatedAt, setLastUpdatedAt] = useState(null);
+
+  const isFirstRender = useRef(false);
+
+  useEffect(() => {
+    // このeffectは初回レンダー時のみ呼ばれるeffect
+    isFirstRender.current = true;
+  }, []);
+
+  useEffect(() => {
+    // 『count』 が更新された場合『と』初回レンダー時に動くeffect
+    if (isFirstRender.current) {
+      // 初回レンダー判定
+      isFirstRender.current = false; // もう初回レンダーじゃないよ代入
+    } else {
+      setLastUpdatedAt(new Date().toString());
+    }
+  }, [count]);
+
+  return (
+    <Area count={count} lastUpdatedAt={lastUpdatedAt} setCount={setCount} />
+  );
+};
+
+const Michael = () => {
+  const [count, setCount] = useState(0);
+  const [lastUpdatedAt, setLastUpdatedAt] = useState(null);
+
+  const isFirstRender = useRef(true);
+
+  useEffect(() => {
+    if (isFirstRender.current) {
+      isFirstRender.current = false;
+    } else {
+      setLastUpdatedAt(new Date().toString());
+    }
+  }, [count]);
+
+  return (
+    <Area count={count} lastUpdatedAt={lastUpdatedAt} setCount={setCount} />
+  );
+};
+
+const Janet = () => {
+  const [count, setCount] = useState(0);
+  const [lastUpdatedAt, setLastUpdatedAt] = useState(null);
+
+  useEffect(() => {
+    if (count === 0) return;
+
+    setLastUpdatedAt(new Date().toString());
+  }, [count]);
+
+  return (
+    <Area count={count} lastUpdatedAt={lastUpdatedAt} setCount={setCount} />
+  );
+};
+
+const App = () => {
+  return (
+    <>
+      <Qiita />
+      <Michael />
+      <Janet />
+    </>
+  );
+};
 
 export default App;
